f6e858a97ead754e35ff3f77fd28a70e269dcb17
Bahar <baharsadeghzade@gmail.com>
2024-01-27 16:16:09 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import generateJoke from "./generateJoke";
+import "./styles/main.scss";
+import laughing from "./assets/laughing.svg";
+
+const laughImg = document.getElementById("laughImg");
+laughImg.src = laughing;
+
+console.log(generateJoke());
+console.log(123);
+console.log(bahar);
